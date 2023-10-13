d3d2a3ca9cb585785d22a01ee4fa2a3fb5c26cb5
woongcheol Shin <5starshin@gmail.com>
2023-10-13 21:42:03 +0900

    feat: function & class props 적용

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import React from 'react';
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return <div className="container">
+    <h1>Hello World</h1>
+    <FuncComp initNumber={2}></FuncComp>
+    <ClassComp initNumber={1}></ClassComp>
+  </div>;
+}
+
+function FuncComp(props) {
+  return(
+    <div className="container">
+      <h2>function style component</h2>
+      <p>Number : {props.initNumber}</p>
+    </div>
+  );
+}
+
+class ClassComp extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h2>class style component</h2>
+        <p>Number : {this.props.initNumber}</p>
+      </div>
+    )
+  }
 }
 
 export default App;
