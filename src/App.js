fd50e0312ec7eeb2626c03b96d3cbdb1fe7a4d20
woongcheol Shin <5starshin@gmail.com>
2023-10-13 22:58:33 +0900

    feat: 클래스 컴포넌트 라이프 사이클 메소드 구현

--- src/App.js
+++ src/App.js
@@ -29,14 +29,61 @@ function FuncComp(props) {
   );
 }
 
+const classStyle = "color:red";
 class ClassComp extends React.Component {
-  // state 초기화
-  state = {
-    number: this.props.initNumber,
-  };
+  // 1. 초기 마운트 시
+  // 1-1. 클래스 생성
+  constructor(props) {
+    console.log("%cclass => constructor", classStyle);
+    super(props);
 
-  // state 업데이트 시 리렌더링
+    // state 초기화
+    this.state = {
+      number: props.initNumber,
+    };
+  }
+
+  // 1-2. 초기 렌더링 시
+  componentDidMount() {
+    console.log("%cclass => componentDidMount", classStyle);
+  }
+
+  // 마운트 & 업데이트) props를 state에 동기화 시
+  static getDerivedStateFromProps() {
+    console.log("%cclass => getDerivedStateFromProps", classStyle);
+    return null;
+  }
+
+  // 2. 업데이트 시
+
+  // 2-1. 업데이트 및 리렌더링 전, 여부 결정 시
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("%cclass => shouldComponentUpdate", classStyle);
+
+    // 특정 조건을 기반으로 리렌더링 여부 결정
+    // if (nextProps.someProp === this.props.someProp) {
+    //   return false;
+    // }
+
+    return true;
+  }
+
+  // 2-2. 업데이트 완료 시
+  componentDidUpdate(prevProps, prevState) {
+    console.log("%cclass => componentDidUpdate", classStyle);
+    console.log("Previous props:", prevProps);
+    console.log("Previous state:", prevState);
+  }
+
+  // 3. 언마운트 시
+  componentWillUnmount() {
+    console.log("%cclass => componentWillUnmount", classStyle);
+    // 여기에서 정리 작업 수행
+  }
+
+  // 마운트 & 업데이트) 랜더링 시
   render() {
+    console.log("%cclass => render", classStyle);
     return (
       <div className="container">
         <h2>class style component</h2>
@@ -44,9 +91,9 @@ class ClassComp extends React.Component {
         <input
           type="button"
           value="random"
-          onClick={function () {
+          onClick={() => {
             this.setState({ number: Math.random() });
-          }.bind(this)}
+          }}
         ></input>
       </div>
     );
